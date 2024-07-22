acc933332edac449e8fd062fbb1b59bf79e33cc3
hema2601 <maike.helbig@onlinehome.de>
2024-07-22 15:59:18 +0900

    Script tweaks + git restructuring

--- run_experiment.sh
+++ run_experiment.sh
@@ -1,6 +1,7 @@
 
 current_path=/home/hema/IPC_Module/
 remote_client_addr=hema@115.145.178.17
+server_ip=10.0.0.3
 
 exp_name=${1:-exp}
 intf=${2:-ens4}
@@ -22,7 +23,7 @@ service irqbalance stop
 ethtool -L $intf rx $num_queue tx $num_queue 
 
 # Set Mappings
-$current_path/set_affinity.sh $intf $core_start
+$current_path/scripts/set_affinity.sh $intf $core_start
 
 # Make and Insert Module
 make -C $current_path/module -f Makefile
@@ -32,14 +33,14 @@ insmod $current_path/module/IPC_module.ko
 gcc accessor.c
 
 # Get "Before" Values
-$current_path/before.sh
+$current_path/scripts/before.sh
 
 # Run iperf3
-taskset -c "$core_start-$((core_start + core_num - 1))" iperf3 -s -1 & ssh $remote_client_addr "iperf3 -c 10.0.0.3 -P ${conns} > /dev/null"&
+taskset -c "$core_start-$((core_start + core_num - 1))" iperf3 -s -1 & ssh $remote_client_addr "iperf3 -c ${server_ip} -P ${conns} > /dev/null"&
 
 # Wait a little
-sleep 2
-echo "Now!"
+sleep 23
+
 # Run accessor
 ./a.out -t $time -c $core_start-$((core_start + core_num - 1)) $exp_name 
 
@@ -47,7 +48,7 @@ echo "Now!"
 
 
 # Get "After" Values
-$current_path/after.sh $exp_name
+$current_path/scripts/after.sh $exp_name
 
 # Apply File Transformation
 python3 file_formatter.py $exp_name IRQ SOFTIRQ PACKET_CNT
